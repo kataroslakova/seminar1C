43ff50e1bd92bf21a74d3adf1697e30c87664b92
Kate <kataroslakova10@gmail.com>
2023-05-22 16:04:09 +0300

    дополнили ветвлением

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -5,5 +5,17 @@
 
 Console.WriteLine("Введите целое число");
 int num = Convert.ToInt32(Console.ReadLine());
-int result = num*num;
-System.Console.WriteLine("Квадрат введенного числа равен " +result);
+int result = num * num;
+System.Console.WriteLine("Квадрат введенного числа равен " + result);
+// git remote add origin https://github.com/kataroslakova/seminar1C.git
+// git branch -M main
+// git push -u origin main
+
+if (num > 0)
+{
+    System.Console.WriteLine("Вы ввели положительное число");
+}
+else if (num == 0)
+    System.Console.WriteLine("Вы ввели ноль");
+else
+    System.Console.WriteLine("Вы ввели отрицательное число");
