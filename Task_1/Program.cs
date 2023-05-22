15ae630dfdaa6a2d8418f3e8d5d11aa7a485da26
Kate <kataroslakova10@gmail.com>
2023-05-22 15:55:19 +0300

    квадрат

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на авход принимает число и выводит его квадрат
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num*num;
+System.Console.WriteLine("Квадрат введенного числа равен " +result);
